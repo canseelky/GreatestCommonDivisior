3f4e5ed70f69e9768efc65c56ff2d05ce92e613e
Cansel Küçükyılmaz <44136479+crossing-over@users.noreply.github.com>
2020-07-07 00:09:00 +0300

    Update gcdLoop.asm

--- gcdLoop.asm
+++ gcdLoop.asm
@@ -22,50 +22,30 @@ main:
 	syscall
 
 	addi $a1,$v0,0
-
-
-
  	li $v0,4			#Display str3
  	la $a0,str3
  	syscall
-
-
-
 	jal gcdLoop			#call gcdLoop
-
-
 
 	addi $a0,$v0,0
 	li $v0,1
-
-
 	syscall
-
 	addi $t1,$v0,0  #return result
-
-
 #exit the program
 	li $v0, 10
 	syscall
 
-
-
 gcdLoop:
-
 	#int number1,number2
 	#int result=0
 	addi $t1,$zero,0    #result==t1=0
 	bge $a1,$a3, swap   #if number2> number 1, set number1
-
 	swap:
 		addi $t6,$a3,0  #number1 stored in t6
 		addi $a3,$a1,0   #number1==NUMBER2
 		addi $a1,$t6,0   #number2==number1
 
-
-
 	do:
-
         	divu $t3,$a3,$a1    #number1/number2
        		mfhi $t2        # remainder for the div.
        		add $a3,$a1,$0   #number1=number2
@@ -75,8 +55,6 @@ gcdLoop:
 
        		j while
        		jr  $ra
-
-
 
 	while:
 		bne $t2,$zero,do
