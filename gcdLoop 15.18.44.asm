3346d20a46ccd6b1f6b1b42fad9e563a4efcc405
Cansel Küçükyılmaz <44136479+crossing-over@users.noreply.github.com>
2020-07-07 00:05:57 +0300

    Add files via upload

---
+++ gcdLoop 15.18.44.asm
@@ -0,0 +1,29 @@
+.data
+str1: .asciiz "Enter an integer: "
+str2: .asciiz "\nEnter another integer: "
+str3: .asciiz "\nThe greatest common divisor is: "
+
+
+.text
+:main
+
+#Request firstinteger from the user
+
+#Request second integer from the user
+
+#call gcdLoop with method with two paremeters
+
+#get the result from the gcdLoop and print it
+
+
+gcdLoop:
+#find the gcd of the given two integers using a loop
+
+
+
+
+
+
+
+
+
